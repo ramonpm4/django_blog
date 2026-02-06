18e15780fa7f879cd233cf7a1e484838e3c8d4dc
Ramón <ramonperaltamartinez@Ramons-MacBook-Air.local>
2026-02-06 15:01:07 -0300

    my_posts list

--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,81 @@
 from django.shortcuts import render
 from django.http import Http404, HttpResponse
+from datetime import date
 
 # Create your views here.
 
 
 
 my_posts = [
-    {'title': 'The real man', 'slug':'the-real-man', 'content': 'This is my first post.'},
-    {'title': 'Why you should learn django.','slug': 'why-to-learn-django' , 'content': 'Why you should learn django.'}
-]
+        {
+            'slug': 'hike-in-the-mountains',
+            'image': 'mountains.jpg',
+            'author': 'Ramon',
+            'date': date(2026, 2, 8),
+            'title': 'Viaje a La Josefina - Saavedra',
+            'excert': 'En este viaje pude conocer en mayor profundiad la estancia La Josefina. Compartir con mi hermano y aprender de ganaderia regenerativa.',
+            'content': """
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum ab voluptates delectus reiciendis esse eos accusamus doloremque 
+                sapiente dolorum placeat ipsam repellendus quia, rem quae aut doloribus deserunt maxime incidunt.
+            
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum ab voluptates delectus reiciendis esse eos accusamus doloremque 
+                sapiente dolorum placeat ipsam repellendus quia, rem quae aut doloribus deserunt maxime incidunt.
+            
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum ab voluptates delectus reiciendis esse eos accusamus doloremque 
+                sapiente dolorum placeat ipsam repellendus quia, rem quae aut doloribus deserunt maxime incidunt.
+            
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum ab voluptates delectus reiciendis esse eos accusamus doloremque 
+                sapiente dolorum placeat ipsam repellendus quia, rem quae aut doloribus deserunt maxime incidunt.
+            
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum ab voluptates delectus reiciendis esse eos accusamus doloremque 
+                sapiente dolorum placeat ipsam repellendus quia, rem quae aut doloribus deserunt maxime incidunt.
+                
+            """
+        },
+        {
+            "slug": "programming-is-fun",
+            "image": "coding.jpg",
+            "author": "Maximilian",
+            "date": date(2022, 3, 10),
+            "title": "Programming Is Great!",
+            "excerpt": "Did you ever spend hours searching that one error in your code? Yep - that's what happened to me yesterday...",
+            "content": """
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+            aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+            velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+            aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+            velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+            aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+            velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+            """
+        },
+        {
+            "slug": "into-the-woods",
+            "image": "woods.jpg",
+            "author": "Maximilian",
+            "date": date(2020, 8, 5),
+            "title": "Nature At Its Best",
+            "excerpt": "Nature is amazing! The amount of inspiration I get when walking in nature is incredible!",
+            "content": """
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+            aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+            velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+            aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+            velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+            aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+            velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+            """
+        }]
+    
+
 
 
 def starting_page(request) -> HttpResponse:
