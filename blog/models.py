d9c1ed1ec361e517c1320988f187225d21a65bbf
Ramón <ramonperaltamartinez@Ramons-MacBook-Air.local>
2026-02-10 19:05:11 -0300

    app admin.py include models

--- blog/models.py
+++ blog/models.py
@@ -2,8 +2,46 @@ from django.db import models
 
 # Create your models here.
 
-class Post(models.Model):
-    title = models.CharField(max_length=50)
-    excerpt = models.CharField(max_length=200)
-    image_name = models.CharField(max_length=50)
+class Author(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    mail_address = models.CharField(max_length=50)
     
+    def full_name(self) -> str:
+        return f"{self.first_name} {self.last_name}"
+    
+    def __str__(self) -> str:
+        return self.full_name()
+    
+    class Meta:
+        verbose_name_plural = 'Authors'
+    
+    
+    
+class Tag(models.Model):
+    caption = models.CharField(max_length=50)
+    
+    def __str__(self) -> str:
+        return self.caption
+    
+    class Meta:
+        verbose_name_plural = 'Tags'
+    
+    
+
+class Post(models.Model):
+    title = models.CharField(max_length=150)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True, related_name='post')
+    excerpt = models.CharField(max_length=500)
+    image_name = models.CharField(max_length=50)
+    date_created = models.DateField(auto_now_add=True)
+    slug = models.SlugField(default="", blank=True,  null=False, db_index=True) 
+    content = models.TextField()
+    tag = models.ManyToManyField(Tag)
+    
+    def __str__(self) -> str:
+        return f"{self.title} ({self.date_created})"
+    
+    class Meta:
+        verbose_name_plural = 'Posts'
+        
