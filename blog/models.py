6eccbf3d57dca7dd6271748b53013a37573bbf91
Ramón <ramonperaltamartinez@Ramons-MacBook-Air.local>
2026-02-10 18:52:49 -0300

    Add Post model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    excerpt = models.CharField(max_length=200)
+    image_name = models.CharField(max_length=50)
+    
